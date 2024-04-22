f2d0dd22c3e69876fa742835aaf5bc8b04b1c5cb
unknown <omar.aomar9819@gmail.com>
2024-04-22 14:16:56 +0100

    Form completed just need styling

--- src/app/registration/single/page.tsx
+++ src/app/registration/single/page.tsx
@@ -9,6 +9,8 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import {
   Select,
   SelectContent,
@@ -21,15 +23,31 @@ import { useForm } from "react-hook-form";
 import * as z from "zod";
 
 const phoneValidation = /^(?:(?:\+|00)44|0)7(?:[45789]\d{2}|624)\d{6}$/;
+const postcodeValidation =
+  /^(GIR|[A-Z]\d[A-Z\d]??|[A-Z]{2}\d[A-Z\d]??)[ ]??(\d[A-Z]{2}|BFPO[ ]?\d{1,4}|(KY\d|MSR|VG|AI)[ ]?\d{1,4}|(AI-2640|KY\d|MSR|VG|AI)[ ]?\d{1,4}|[A-Z]{2}|GEICO[ ]?\d{1,4}|[A-Z]{2}[ ]?\d{1,4})$/i;
 
 const formSchema = z.object({
-  name: z.string(),
+  firstName: z.string().min(3, { message: "Minimum 3 characters" }),
+  middleName: z.string(),
+  lastName: z.string().min(3, { message: "Minimum 3 characters" }),
   emailAddress: z.string().email(),
   accountType: z.enum(["single", "family"]),
+  title: z.enum(["mr", "miss", "ms", "mrs", "dr"]),
   contactNumber: z
     .string()
     .regex(phoneValidation, { message: "Invalid UK phone number" }),
   nationality: z.string(),
+  postcode: z
+    .string()
+    .regex(postcodeValidation, { message: "Please enter a valid UK Postcode" }),
+  address: z.string(),
+  emergencyName: z.string().min(3),
+  emergencyNumber: z
+    .string()
+    .regex(phoneValidation, { message: "Invalid UK phone number" }),
+  hospitalised: z.boolean(),
+  benefits: z.boolean(),
+  resident: z.boolean(),
   spouseName: z.string().optional(),
   children: z.array(z.object({ name: z.string().min(3) })).optional(),
 });
@@ -38,10 +56,7 @@ export default function SingleRegistration() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      emailAddress: "",
-      accountType: "single", // Set default account type
-      spouseName: "", // Initialize spouse name
-      children: [{ name: "" }], // Initialize children array with an empty object
+      accountType: "single",
     },
   });
 
@@ -50,11 +65,11 @@ export default function SingleRegistration() {
   };
 
   return (
-    <main className="flex min-h-screen col items-center justify-between p-24">
+    <main className="flex min-h-screen justify-between p-24">
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(handleSubmit)}
-          className="max-w w-full flex flex-col gap-4"
+          className="flex flex-col gap-4 "
         >
           <FormField
             control={form.control}
@@ -79,14 +94,105 @@ export default function SingleRegistration() {
               </FormItem>
             )}
           />
+          <div className="w-full flex flex-row gap-5">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Title</FormLabel>
+                  <Select onValueChange={field.onChange}>
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Title" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="mr">Mr</SelectItem>
+                      <SelectItem value="miss">Miss</SelectItem>
+                      <SelectItem value="mrs">Mrs</SelectItem>
+                      <SelectItem value="ms">Ms</SelectItem>
+                      <SelectItem value="dr">Dr</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="firstName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>First Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="First Name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="middleName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Middle Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Middle Name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="lastName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Last Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Your Last Name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
           <FormField
             control={form.control}
-            name="name"
+            name="nationality"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Name</FormLabel>
+                <FormLabel>Nationality</FormLabel>
                 <FormControl>
-                  <Input placeholder="Your Name" {...field} />
+                  <Input placeholder="Nationality" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="address"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Address</FormLabel>
+                <FormControl>
+                  <Input placeholder="Address" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="postcode"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Postcode</FormLabel>
+                <FormControl>
+                  <Input placeholder="Postcode" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -99,11 +205,7 @@ export default function SingleRegistration() {
               <FormItem>
                 <FormLabel>Email Address</FormLabel>
                 <FormControl>
-                  <Input
-                    placeholder="Email Address"
-                    type="emailAddress"
-                    {...field}
-                  />
+                  <Input placeholder="Email Address" type="email" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -116,11 +218,110 @@ export default function SingleRegistration() {
               <FormItem>
                 <FormLabel>Contact Number</FormLabel>
                 <FormControl>
+                  <Input placeholder="07123456789" type="tel" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="emergencyName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Emergency contact's name</FormLabel>
+                <FormControl>
                   <Input
-                    placeholder="Contact Number"
-                    type="contactNumber"
+                    placeholder="Emergency Contact Name"
+                    type="emergencyName"
                     {...field}
                   />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="emergencyNumber"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Contact Number</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Emergency Contact Number"
+                    type="tel"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="hospitalised"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Are you in care?</FormLabel>
+                <FormControl>
+                  <RadioGroup defaultValue="yes">
+                    <div className="flex items-center space-x-2">
+                      <RadioGroupItem value="yes" id="r1" />
+                      <Label htmlFor="r1">Yes</Label>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                      <RadioGroupItem value="no" id="r2" />
+                      <Label htmlFor="r2">No</Label>
+                    </div>
+                  </RadioGroup>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="benefits"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+                  Are you receiving benefits i.e. unviversal credit, child tax
+                  credit
+                </FormLabel>
+                <FormControl>
+                  <RadioGroup defaultValue="yes">
+                    <div className="flex items-center space-x-2">
+                      <RadioGroupItem value="yes" id="r1" />
+                      <Label htmlFor="r1">Yes</Label>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                      <RadioGroupItem value="no" id="r2" />
+                      <Label htmlFor="r2">No</Label>
+                    </div>
+                  </RadioGroup>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="resident"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Are you a resident of the UK</FormLabel>
+                <FormControl>
+                  <RadioGroup defaultValue="yes">
+                    <div className="flex items-center space-x-2">
+                      <RadioGroupItem value="yes" id="r1" />
+                      <Label htmlFor="r1">Yes</Label>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                      <RadioGroupItem value="no" id="r2" />
+                      <Label htmlFor="r2">No</Label>
+                    </div>
+                  </RadioGroup>
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -129,6 +330,7 @@ export default function SingleRegistration() {
           <Button type="submit" className="w-full">
             Submit
           </Button>
+          <p>By pressing Submit you agree to our Rules</p>
         </form>
       </Form>
     </main>
